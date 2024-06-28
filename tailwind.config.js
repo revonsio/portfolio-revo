280687cc84a364f73c5d33d67a3cbd57fedb5ae9
revonsio <revonsio12@gmail.com>
2024-06-28 10:25:18 +0700

    first comm

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        kuning: '#F1C21B',
+      },
+      screens: {
+        
+      },
+    },
+  },
+  plugins: [],
+};
